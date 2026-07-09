6448f3cacbfc041864f25034b1df982f1c86c7f8
cozilic <cozilic@gmail.com>
2026-07-09 02:13:17 +0200

    Allow official theme submissions without CSRF

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+        $middleware->preventRequestForgery(except: [
+            'themes/submissions',
+        ]);
 
         $middleware->web(append: [
             SetUserLocale::class,
